8244a126428cfce5214bb98e41c4653b54fb1d8d
juli868 <bugijul@gmail.com>
2024-03-19 14:14:52 +0200

    Only int as parameters

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int):
     """Perfom await n times."""
     tasks = [wait_random(max_delay) for _ in range(n)]
     _, completed = await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
