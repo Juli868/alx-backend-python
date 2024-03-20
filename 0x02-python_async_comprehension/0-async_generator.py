1852b0e1fcaf9694f61c7bc7064cddbb62565e45
juli868 <bugijul@gmail.com>
2024-03-20 14:55:07 +0200

    Function anotations correction

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Wait and tield a number."""
 import asyncio
+from typing import Generator
 import random
 
 
-async def async_generator() -> list[float]:
+async def async_generator() -> Generator[float, None, None]:
     """Generate and yield a number after a sleep."""
     for i in range(10):
         await asyncio.sleep(1)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Utilise yield results in the new function."""
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """Iterate over the yielded number."""
     result = [num async for num in async_generator()]
     return result
