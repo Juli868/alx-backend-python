074cf23358dc9558f9442193e6ff999e838162fd
juli868 <bugijul@gmail.com>
2024-03-20 14:57:54 +0200

    async.gather.

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,11 +8,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Find the time used in processing."""
     start_time = time.time()
-    await asyncio.gather(
-            async_comprehension(),
-            async_comprehension(),
-            async_comprehension(),
-            async_comprehension())
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
 
     end_time = time.time()
     return end_time - start_time
